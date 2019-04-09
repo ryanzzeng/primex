44582b537afdb9318e0ca87e9535b9133ec3aca1
ryan <zengruijiang@gmail.com>
2019-04-09 18:45:58 +1000

    allow to delete multiple users

---
+++ app/Core/Users/Requests/DeleteUserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Core\Users\Requests;
+
+use App\Core\Base\BaseFormRequest;
+
+class DeleteUserRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'user_ids' => ['required','array'],
+        ];
+        return $rules;
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'user_ids.required' => 'The :attribute field is missing',
+        ];
+    }
+}
---
+++ app/Core/Users/Requests/ShowUserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Core\Users\Requests;
+
+use App\Core\Base\BaseFormRequest;
+
+class ShowUserRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => ['required', 'exists:users,id'],
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'user_id.required' => 'The :attribute field is missing',
+            'user_id.exists' => 'Invalid user id',
+        ];
+    }
+}
